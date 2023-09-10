6b316d420f4ca2c0c5d0f9c8c86fbc7c3980e01f
gavinlai999 <136124137+gavinlai999@users.noreply.github.com>
2023-09-10 15:45:45 -0700

    Update index.js
    GAVIN CHANGED IT

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const gavinFunction = () =>{
   const userQuestion = 'Do you like me? ';
+  const userAnswer = "Yes";
   const input = prompt(userQuestion);
   const answer= `Why ${input}??`;
-  console.log(answer);
+  const isCorrect = input && input.toLowerCase() === userAnswer.toLowerCase();
+
+  console.log(isCorrect);
 };
 
 gavinFunction();
