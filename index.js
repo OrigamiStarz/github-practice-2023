9e2e58f28bcd1e28da2016ce1e92fe11a4990694
gavinlai999 <136124137+gavinlai999@users.noreply.github.com>
2023-09-10 16:03:33 -0700

    Update index.js
    fix now

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const gavinFunction = () =>{
   const userQuestion = 'Do you like me? ';
   const userAnswer = "Yes";
   const input = prompt(userQuestion);
-  const answer= `Why ${input}??`;
+  const answer= `Why ${input}t??`;
   const isCorrect = input && input.toLowerCase() === userAnswer.toLowerCase();
+const resultMessage = isCorrect ? `Nice! "${userAnswer}" is correct. `: `Oops! "${answer} ${userAnswer}".`;
+console.log(resultMessage);
 
-  console.log(isCorrect);
 };
 
 gavinFunction();
